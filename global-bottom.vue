987ec93574827995184c031f945b31144e2e4b77
Ruslee Sutthaweekul <ruslylove@gmail.com>
2025-07-06 18:17:31 +0700

    add page number

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,5 +1,5 @@
 <!-- global-bottom.vue -->
 <template>
-    <footer class="absolute bottom-0 right-0 p-2 opacity-40">010153523 Algorithms and Data Structures 1/2025 </footer>
+    <footer class="absolute bottom-0 right-0 p-2 opacity-40">010153523 Algorithms and Data Structures 1/2025 - Page {{$page}}</footer>
     <footer class="absolute bottom-0 left-0 p-2 opacity-40"> <img src="https://ece.eng.kmutnb.ac.th/en/wp-content/uploads/2023/10/cropped-cropped-ECET-Shirt.png" style="height:20px;position:fixed;left:115px">ECE KMUTNB</footer>
   </template>
